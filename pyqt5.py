49962314557cb4fc4c405e06b166d3f7efc5712a
Patricio <patricio.afr@gmail.com>
2023-01-04 02:28:41 -0300

    test with pyqt5 buttons

--- pyqt5.py
+++ pyqt5.py
@@ -1,1 +1,9 @@
-import PyQt5.QtWidgets as qtw
+import sys
+from PyQt5.QtWidgets import QApplication, QPushButton
+
+app = QApplication(sys.argv)
+
+window = QPushButton("Push Me")
+window.show()
+
+app.exec()
