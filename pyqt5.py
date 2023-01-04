3f40d7d3f0cb4db15eb1fffe1e35938788f51176
Patricio <patricio.afr@gmail.com>
2023-01-04 03:07:04 -0300

    first try with a vertical layout, some labels, a button and an event

--- pyqt5.py
+++ pyqt5.py
@@ -1,9 +1,53 @@
-import sys
-from PyQt5.QtWidgets import QApplication, QPushButton
+import PyQt5.QtWidgets as qtw
 
-app = QApplication(sys.argv)
+import PyQt5.QtGui as qtg
 
-window = QPushButton("Push Me")
-window.show()
+class MainWindow(qtw.QWidget):
+    def __init__(self):
+        super().__init__()
+        
+        #Add title
+        self.setWindowTitle("App-Consultora")
 
-app.exec()
+        # Set vertical layout
+        self.setLayout(qtw.QVBoxLayout())
+
+        #Create label
+
+        load_label = qtw.QLabel("Introduzca su base de datos")
+
+        #Change fontsize of label
+        load_label.setFont(qtg.QFont("Helvetica", 12))
+
+        self.layout().addWidget(load_label)
+
+        #Create an entry box
+
+        my_entry =qtw.QLineEdit()
+        my_entry.setObjectName("csv_file")
+        self.layout().addWidget(my_entry)
+
+        #Create button
+
+        my_button = qtw.QPushButton("Cargar archivo",
+            clicked = lambda: press_it())
+        self.layout().addWidget(my_button)
+
+        loaded_label = qtw.QLabel("No ha cargado ningún archivo")
+        loaded_label.setFont(qtg.QFont("Helvetica", 12))
+        self.layout().addWidget(loaded_label)
+           
+
+        def press_it():
+            loaded_label.setText(f"El archivo <{my_entry.text()}> se ha cargado con éxito")
+            
+
+        self.show()
+
+
+app= qtw.QApplication([])
+
+mw = MainWindow()
+
+
+app.exec_()
