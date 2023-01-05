e29bc1b27ee35a450dacc22fdebb420f0cdc5ef5
Patricio <patricio.afr@gmail.com>
2023-01-05 03:21:56 -0300

    Manage to add a button, but can't a function to work on click. I think it may be related to an indentation problem

--- pyqt5.py
+++ pyqt5.py
@@ -1,5 +1,5 @@
 import PyQt5.QtWidgets as qtw
-
+import pandas as pd
 import PyQt5.QtGui as qtg
 
 class MainWindow(qtw.QWidget):
@@ -40,6 +40,24 @@ class MainWindow(qtw.QWidget):
 
         def press_it():
             loaded_label.setText(f"El archivo <{my_entry.text()}> se ha cargado con éxito")
+
+            # Create a button and set its text
+        load_btn = qtw.QPushButton('Import CSV', self)
+        load_btn.move(20, 20)
+        self.layout().addWidget(load_btn)
+
+        # Connect the clicked signal to the import_csv method
+        load_btn.clicked.connect(self.import_csv)
+
+    def import_csv(self):
+        options = qtw.QFileDialog.Options()
+        options |= qtw.QFileDialog.ReadOnly
+        file_name, _ = qtw.QFileDialog.getOpenFileName(self,"QFileDialog.getOpenFileName()", "","All Files (*);;CSV Files (*.csv)", options=options)
+        if file_name:
+            # Read the CSV file into a DataFrame using pandas
+            self.df = pd.read_csv(file_name, sep = ";")
+            # Print the DataFrame to the console
+            print(self.df.head())
             
 
         self.show()
